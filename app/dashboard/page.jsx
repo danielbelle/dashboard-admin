0904dfcb53a9e6dd0f27d1ab40ac387b9b98fd64
danielbelle <henrique.danielb@gmail.com>
2024-05-07 18:21:17 -0300

    added sidebar, and menu links

---
+++ app/dashboard/page.jsx
@@ -0,0 +1,15 @@
+import styles from "../ui/dashboard/dashboard.module.css";
+
+const Dashboard = () => {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.main}>
+        Dashboard
+      </div>
+      <div className={styles.side}>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
